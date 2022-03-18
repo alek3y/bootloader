f6119faf942545b128e5c2cd75fafbcb0bea5f97
alek3y <alek3y@protonmail.com>
2022-03-18 15:10:14 +0100

    Add sections to code

--- src/main.asm
+++ src/main.asm
@@ -1,14 +1,17 @@
 [bits 16]
 [org 0x7c00+512]
-
 jmp main	; Avoid include files execution
 
 %include "io/putchar.asm"
 
+section .data
+face: db ":)"
+
+section .text
 main:
-	mov al, 'H'
+	mov al, [face]
 	call putchar
-	mov al, 'i'
+	mov al, [face+1]
 	call putchar
 
 	jmp $
